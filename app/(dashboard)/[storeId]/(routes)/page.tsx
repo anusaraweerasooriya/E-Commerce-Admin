b5b0a63bf9fc1a48ca82fb1269e568cacb879044
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-08-17 16:44:37 +0530

    Passed the params props to the dashboard

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,9 +1,24 @@
+import prismadb from '@/lib/prismadb'
 import React from 'react'
 
+interface DashboardPageProps {
+  params: { storeId: string }
+};
 
-const DashboardPage = () => {
+const DashboardPage:React.FC<DashboardPageProps> = async ({
+  params
+}) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId
+    }
+  });
+
+
   return (
-    <div>DashboardPage</div>
+    <div>
+      Active Store: {store?.name}
+    </div>
   )
 }
 
